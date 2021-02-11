8ff4cca5a7e8a3a6b0583522ea785d9a0e0a3a4b
unknown <you@example.com>
2021-02-11 20:08:13 +0330

    added winsatoshi and wallet page

--- lib/screens/wallet.dart
+++ lib/screens/wallet.dart
@@ -1,19 +1,12 @@
-import 'package:clinkoin/screens/winsatoshi_risk_free.dart';
 import 'package:clinkoin/widgets/shared_long_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ForcastBitcoin extends StatelessWidget {
-  static final routeName = '/forcast_bitcoin';
+class Wallet extends StatelessWidget {
+  Function _changeScreen() {}
+  static final routeName = '/Wallet';
   @override
   Widget build(BuildContext context) {
-    void _changeScreen(BuildContext context) {
-      Navigator.of(context).pushNamedAndRemoveUntil(
-        WinsatoshiRiskFree.routeName,
-        ModalRoute.withName(WinsatoshiRiskFree.routeName),
-      );
-    }
-
     return Scaffold(
       backgroundColor: Color.fromRGBO(245, 245, 253, 1),
       body: Column(
@@ -39,48 +32,48 @@ class ForcastBitcoin extends StatelessWidget {
                     Container(
                       margin: EdgeInsets.only(
                           top: MediaQuery.of(context).size.height * .07),
-                      child: Image.asset('assets/images/chart.png'),
+                      child: Image.asset('assets/images/wallet.png'),
                     ),
                     Text(
-                      'Win Bitcoin',
+                      'Wallet',
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
                     ),
                     Text(
-                      'Eearn Bitcoin with true predicts.You can earn more by play more.',
+                      'You can get your Rewards in a real bitcoin wallet.',
                       textAlign: TextAlign.center,
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Container(
+                          width: 8,
+                          height: 8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color.fromRGBO(196, 196, 196, 1),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Container(
+                          width: 8,
+                          height: 8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color.fromRGBO(196, 196, 196, 1),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Container(
                           width: 32,
                           height: 8,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             color: Color.fromRGBO(41, 114, 255, 1),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Container(
-                          width: 8,
-                          height: 8,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Color.fromRGBO(196, 196, 196, 1),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Container(
-                          width: 8,
-                          height: 8,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Color.fromRGBO(196, 196, 196, 1),
                           ),
                         ),
                       ],
@@ -106,7 +99,7 @@ class ForcastBitcoin extends StatelessWidget {
               alignment: Alignment.bottomCenter,
               child: SharedLongButton(
                 button: _changeScreen,
-                buttonText: 'Next',
+                buttonText: 'Lets try your first Predict',
               ),
             ),
           ),
