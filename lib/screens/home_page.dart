38ace89764c95721a7064d431d70b07eca73e315
unknown <you@example.com>
2021-02-21 08:37:36 +0330

    added feedback

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:clinkoin/screens/predicted_undo.dart';
+import 'package:clinkoin/widgets/shared_long_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<bool> checkButton = [true, false, false, false];
+  final List<bool> checkButton = [true, false, false, false];
   void fCheckButton(int num) {
     setState(() {
       for (var i = 0; i < checkButton.length; i++) {
@@ -19,6 +20,439 @@ class _HomePageState extends State<HomePage> {
       }
       checkButton[num] = true;
     });
+  }
+
+  void _changeScreen(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final args = (ModalRoute.of(context).settings.arguments as Map);
+
+      if (args['showModal'] == true)
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Dialog(
+              insetPadding: EdgeInsets.symmetric(horizontal: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: EdgeInsets.only(right: 16, left: 16, bottom: 24),
+                width: MediaQuery.of(context).size.width * 1,
+                height: 570,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 16),
+                      alignment: Alignment.centerRight,
+                      child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Image.asset('assets/images/dialog-close.png')),
+                    ),
+                    Image.asset('assets/images/alien.png'),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .05,
+                    ),
+                    Text(
+                      'Success!',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: MyApp.twentyOne),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .03,
+                    ),
+                    Text(
+                      'You correctly predicted',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: ' that Bitcoin would go',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 16,
+                            ),
+                          ),
+                          TextSpan(
+                              text: ' up',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(32, 174, 138, 1),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16))
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .03,
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 26, vertical: 11),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Color.fromRGBO(217, 217, 217, 1),
+                        ),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Bitcoin went up ',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 16),
+                            ),
+                            TextSpan(
+                                text: '\$667.55',
+                                style: TextStyle(
+                                    color: Color.fromRGBO(32, 174, 138, 1),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16))
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .05,
+                    ),
+                    FittedBox(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * .05,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Price at Forecast',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Color.fromRGBO(81, 81, 81, 1)),
+                              ),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                '\$ 23,472.97',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                    color: Color.fromRGBO(81, 81, 81, 1)),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * .05,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Current Price',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Color.fromRGBO(81, 81, 81, 1)),
+                              ),
+                              SizedBox(
+                                height: 4,
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    '\$ 24,472.97',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                        color: Color.fromRGBO(32, 174, 138, 1)),
+                                  ),
+                                  SizedBox(
+                                    width: 6,
+                                  ),
+                                  Container(
+                                    child: Image.asset(
+                                        'assets/images/green-polygon.png'),
+                                    margin: EdgeInsets.only(top: 10),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * .05,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          height: 90,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              FittedBox(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'You are currently gaining',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      width: 6,
+                                    ),
+                                    Text('+220 SATOSHI',
+                                        style: TextStyle(
+                                            color:
+                                                Color.fromRGBO(248, 157, 46, 1),
+                                            fontWeight: FontWeight.bold)),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              SharedLongButton(
+                                button: _changeScreen,
+                                buttonText: 'Onward & Upwards',
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            insetPadding: EdgeInsets.symmetric(horizontal: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Container(
+              padding: EdgeInsets.only(right: 16, left: 16, bottom: 24),
+              width: MediaQuery.of(context).size.width * 1,
+              height: 570,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 16),
+                    alignment: Alignment.centerRight,
+                    child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Image.asset('assets/images/dialog-close.png')),
+                  ),
+                  Image.asset('assets/images/sad-alien.png'),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .05,
+                  ),
+                  Text(
+                    'Opps!',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: MyApp.twentyOne),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .03,
+                  ),
+                  Text(
+                    'You incorrectly predicted',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: ' that Bitcoin would go',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                        TextSpan(
+                            text: ' up',
+                            style: TextStyle(
+                                color: Color.fromRGBO(32, 174, 138, 1),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16))
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .03,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 26, vertical: 11),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color.fromRGBO(217, 217, 217, 1),
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Bitcoin went down ',
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                          TextSpan(
+                              text: '\$667.55',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(249, 48, 128, 1),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16))
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .05,
+                  ),
+                  FittedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * .05,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Price at Forecast',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromRGBO(81, 81, 81, 1)),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              '\$ 23,472.97',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  color: Color.fromRGBO(81, 81, 81, 1)),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * .05,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Current Price',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromRGBO(81, 81, 81, 1)),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '\$ 24,472.97',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                    color: Color.fromRGBO(249, 48, 128, 1),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 6,
+                                ),
+                                Container(
+                                  child: Image.asset(
+                                      'assets/images/red-polygon.png'),
+                                  margin: EdgeInsets.only(top: 10),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * .05,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        height: 90,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            FittedBox(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'You incorrectly predicted, win next time',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            SharedLongButton(
+                              button: _changeScreen,
+                              buttonText: 'Predict Again',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    });
+
+    super.initState();
   }
 
   @override
